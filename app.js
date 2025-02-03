454132c68fb995ad43e0235a491f2b73da2f82bb
OmKeshri <omkeshri21@gmail.com>
2025-02-04 00:29:14 +0530

    cors error fixed

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  maxAge: 600,
+  credentials: true,
+}));
 
 // Use the routes
 // app.use("/api/razorpay", razorpayRoutes); // Prefix for Razorpay routes
